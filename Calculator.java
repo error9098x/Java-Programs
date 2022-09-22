e46c1ad63a8701e64bc820af32d933d82a505864
error9098x <43810146+error9098x@users.noreply.github.com>
2022-09-22 14:54:37 +0530

    With This
    :)

--- Calculator.java
+++ Calculator.java
@@ -1,27 +1,37 @@
 import java.util.Scanner;
 class Calculator {
-    public static void add(double num1 , double num2){
+    
+        public double num1; // Member Variables 
+        public double num2; // Member Variables
+    Calculator(double num1 ,double num2) // parameterized constructor
+    {
+        this.num1 = num1; //use of this keyword
+        this.num2 = num2;
+    }
+     void add(){
+        
         System.out.println(" Ans : "+(num1+num2));
         System.out.println("=================================");
     }
 
-    public static void substraction(double num1 , double num2){
+      void substraction(){
         System.out.println(" Ans : "+(num1-num2));
         System.out.println("=================================");
     }
 
-    public static void multiply(double num1 , double num2){
+     void multiply(){
         System.out.println(" Ans : "+(num1*num2));
         System.out.println("=================================");
     }
 
-    public static void divide(double num1 , double num2){
+     void divide(){
         System.out.println(" Ans : "+(num1/num2));
         System.out.println("=================================");
     }
-
+    
+  
     public static void main(String[] args)
-    {
+    {  
         Scanner sc = new Scanner(System.in);
         System.out.println("=================================");
         System.out.println("    Welcome To My Calculator");
@@ -29,21 +39,23 @@ class Calculator {
         System.out.println("    Enter the two numbers :-");
         double num1 = sc.nextDouble();
         double num2 = sc.nextDouble();
+        Calculator obj = new Calculator(num1,num2);
+       
         System.out.println("=================================");
         System.out.println("Enter 1 for Add \n      2 for Substraction \n      3 for Multiplication \n      4 for Division");
         System.out.println("=================================");
         int option = sc.nextInt();
         if (option == 1){
-            add(num1 , num2);
+            obj.add();
         }
         else if (option == 2){
-            substraction(num1 , num2);
+            obj.substraction();
         }
         else if (option == 3){
-            multiply(num1 , num2);
+            obj.multiply();
         }
         else if (option == 4){
-            divide(num1 , num2);
+            obj.divide();
         }
         else if (option != 1 || option != 2 || option != 3 || option != 4)
         {
@@ -52,5 +64,6 @@ class Calculator {
             System.out.println("Program is Ending....... Bye");
             System.out.println("=================================");
         }
-    }
-}
+    }}
+
+
